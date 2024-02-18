4b9c5dd04da91e64f31b72b25be9d70ac6b10619
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2024-02-18 14:31:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,7 +57,7 @@ if len(LOG_CHANNEL) == 0:
     exit()
 else:
     LOG_CHANNEL = int(LOG_CHANNEL)
-IS_FSUB = is_enabled('IS_FSUB', True)
+IS_FSUB = is_enabled('IS_FSUB', )
 
 # support group
 SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1001955515603')
